7dfeee767bf30f4c0433064c22e03ab9d8a1925b
msdiean <168319996+msdiean@users.noreply.github.com>
2025-09-06 23:31:16 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-set -e 
-docker pull msdiean/sample-python-flask-app:latest
-docker run -d --name flask-app -p 5000:5000 msdiean/sample-python-flask-app:latest 
+set -e
 
+# Stop and remove any container using port 5000
+existing_container=$(docker ps -q --filter "publish=5000")
+
+if [ -n "$existing_container" ]; then
+  echo "Stopping container using port 5000..."
+  docker stop $existing_container
+  docker rm $existing_container
+fi
+
+# Run the new container
+docker run -d --name flask-app -p 5000:5000 abhishekf5/simple-python-flask-app:latest
