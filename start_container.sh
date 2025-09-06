3ecf4e8aacc6c904da63084a0031d923f01f7767
msdiean <168319996+msdiean@users.noreply.github.com>
2025-09-06 23:40:16 +0530

    Create start_container.sh

---
+++ start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Always pull latest image
+docker pull abhishekf5/simple-python-flask-app:latest
+
+# Start new container
+docker run -d --name flask-app -p 5000:5000 abhishekf5/simple-python-flask-app:latest
